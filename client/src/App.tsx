b8b343310732c25217c6270c8fdac2c7260e34d2
Ryan Kulyassa <rkulyassa@gmail.com>
2026-03-16 17:36:09 -0400

    backend structuring, proof of concept game loop, keyDown handling on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,46 @@
 import { useEffect, useRef } from "react";
-import { Application, Container, Graphics } from "pixi.js";
+import { Application, /*Container,*/ Graphics } from "pixi.js";
 
 function App() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const appRef = useRef<Application | null>(null);
-  const mouseCircleRef = useRef<Graphics | null>(null);
+  // const mouseCircleRef = useRef<Graphics | null>(null);
+  const wsRef = useRef<WebSocket | null>(null);
+
+  const initPixi = async () => {
+    const app = new Application();
+
+    await app.init({
+      backgroundColor: 0x1e1e2e,
+      resizeTo: containerRef.current || undefined,
+    });
+
+    // if (containerRef.current) {
+    //   containerRef.current.innerHTML = ""; // fix race condition
+    //   containerRef.current.appendChild(app.canvas);
+    // }
+
+    // appRef.current = app;
+
+    // const circle = new Graphics().circle(0, 0, 50).fill(0xcba6f7);
+    // if (appRef.current) {
+    //   appRef.current.stage.addChild(circle);
+    //   mouseCircleRef.current = circle;
+    // }
+  };
+
+  const initWS = async () => {
+    wsRef.current = new WebSocket("ws://localhost:9001");
+  };
 
   useEffect(() => {
-    const initPixi = async () => {
-      const app = new Application();
-
-      await app.init({
-        backgroundColor: 0x1e1e2e,
-        resizeTo: containerRef.current || undefined,
-      });
-
-      if (containerRef.current) {
-        containerRef.current.innerHTML = ""; // fix race condition
-        containerRef.current.appendChild(app.canvas);
-      }
-
-      appRef.current = app;
-
-      const circle = new Graphics().circle(0, 0, 50).fill(0xcba6f7);
-      if (appRef.current) {
-        appRef.current.stage.addChild(circle);
-        mouseCircleRef.current = circle;
-      }
-    };
-
     initPixi();
+    initWS();
+
+    // auto-focus the container for keydown events
+    const container = containerRef.current;
+    if (!container) return;
+    container.focus();
 
     return () => {
       if (appRef.current) {
@@ -39,20 +50,29 @@ function App() {
     };
   }, []);
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const app = appRef.current;
-    if (!app || !mouseCircleRef.current) return;
+  // const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+  //   const app = appRef.current;
+  //   if (!app || !mouseCircleRef.current) return;
 
-    mouseCircleRef.current.x = event.clientX;
-    mouseCircleRef.current.y = event.clientY;
+  //   mouseCircleRef.current.x = event.clientX;
+  //   mouseCircleRef.current.y = event.clientY;
+  // };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(event.key);
+    const ws = wsRef.current;
+    if (!ws) return;
+
+    ws.send(event.key);
   };
 
   return (
     <>
       <div
+        tabIndex={0} // make focusable for use with keydown events
         className="h-screen"
         ref={containerRef}
-        onMouseMove={handleMouseMove}
+        onKeyDown={handleKeyDown}
+        // onMouseMove={handleMouseMove}
       />
     </>
   );
--- gameserver/src/main.cpp
+++ gameserver/src/main.cpp
@@ -2,50 +2,88 @@
 #include <chrono>
 #include <thread>
 #include <vector>
-#include <optional>
 #include <App.h>
 
-typedef struct PlayerData {
-    float x;
-    float y;
-} Player;
+constexpr int GAME_TICK_PERIOD = 50; // ms
+constexpr int WORLD_GRID_WIDTH = 10;
+constexpr int WORLD_GRID_HEIGHT = 10;
 
-constexpr int GAME_TICK_PERIOD = 50; // tick period in ms
+struct PerSocketData {};
+typedef uWS::WebSocket<false, true, PerSocketData> WebSocket;
 
-
-// typedef struct GameState {
-//     std::vector<Player> players;
-//     int gridWidth;
-//     int gridHeight;
-//     int count;
-// } GameState;
-
-// GameState s{
-//     .players = {},
-//     .gridWidth = 20,
-//     .gridHeight = 15,
-//     .count = 0
-// };
-int tickCount = 0;
-
-enum class ClientOpcode {
-    JOIN,
-    LEAVE
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 };
 
-typedef uWS::WebSocket<false, true, PlayerData> WebSocket;
+typedef struct Player {
+    uint8_t pid;
+    int r;
+    int c;
+    Direction direction;
+} Player;
 
-// typedef struct InputEvent {
-//     WebSocket* client;
-//     ClientOpcode opcode;
-//     std::optional<std::string> msg;
-// } InputEvent;
+uint8_t pidIndex = 0;
+
+typedef struct Tile {
+    Player *owner;
+} Tile;
+
+typedef struct World {
+    std::unordered_map<WebSocket*, Player> players;
+    Tile grid[WORLD_GRID_HEIGHT][WORLD_GRID_WIDTH];
+} World;
+
+World world{};
+
 uWS::App *globalApp;
 
-void gameTick(us_timer_t*) {
-    globalApp->publish("game", "tick " + std::to_string(tickCount++), uWS::OpCode::TEXT, false);
-}
+void gameTick(us_timer_t *) {
+    // step each player based on their current direction
+    for (auto& [ws, player] : world.players) {
+        Tile *currentTile = &world.grid[player.r][player.c];
+        currentTile->owner = nullptr;
 
+        int newR = player.r;
+        int newC = player.c;
+
+        switch (player.direction) {
+            case Direction::UP:
+                newR--;
+                break;
+            case Direction::DOWN:
+                newR++;
+                break;
+            case Direction::LEFT:
+                newC--;
+                break;
+            case Direction::RIGHT:
+                newC++;
+                break;
+        }
+
+        
+        newR = std::max(0, std::min(newR, WORLD_GRID_HEIGHT - 1));
+        newC = std::max(0, std::min(newC, WORLD_GRID_WIDTH - 1));
+
+        Tile *newTile = &world.grid[newR][newC];
+        newTile->owner = &player;
+    }
+
+    // Create a flattened state packet of the grid: each cell encodes the player id or empty
+    std::string statePacket;
+    for (int r = 0; r < WORLD_GRID_HEIGHT; r++) {
+        for (int c = 0; c < WORLD_GRID_WIDTH; c++) {
+            Tile *tile = &world.grid[r][c];
+            Player *owner = tile ? tile->owner : nullptr;
+            statePacket += owner ? std::to_string(owner->pid) + "," : "0,";
+        }
+    }
+    // std::cout << statePacket << std::endl;
+    globalApp->publish("game", statePacket, uWS::OpCode::TEXT, false);
+}
 
 int main() {
 
@@ -73,16 +111,40 @@ int main() {
 
     uWS::App app;
 
-    app.ws<PlayerData>("/*", {
+    app.ws<PerSocketData>("/*", {
         .open = [](WebSocket* ws) {
-            // std::cout << "Client connected" << std::endl;
+            std::cout << "Client connected" << std::endl;
             // ws->send("Hello", uWS::OpCode::TEXT);
             ws->subscribe("game");
+
+            Player player = Player{
+                .pid = pidIndex++,
+                .r = 0,
+                .c = 0,
+                .direction = Direction::RIGHT
+            };
+
+            world.players[ws] = player;
         },
         .message = [](WebSocket* ws, std::string_view rawMessage, uWS::OpCode opCode) {
-            float dx;
-            float dy;
-            sscanf(rawMessage.data(), "%f %f", &dx, &dy);
+            std::string message = std::string(rawMessage);
+
+            Player &player = world.players[ws];
+
+            if (message == "ArrowUp") {
+                player.direction = Direction::UP;
+            } else if (message == "ArrowDown") {
+                player.direction = Direction::DOWN;
+            } else if (message == "ArrowLeft") {
+                player.direction = Direction::LEFT;
+            } else if (message == "ArrowRight") {
+                player.direction = Direction::RIGHT;
+            } else {
+                return;
+            }
+            // float dx;
+            // float dy;
+            // sscanf(rawMessage.data(), "%f %f", &dx, &dy);
             // std::cout << "Received message: " << std::string(raw_message) << std::endl;
             // std::cout << "Received message: " << raw_message.data() << std::endl;
             // std::cout << dx << dy << std::endl;
